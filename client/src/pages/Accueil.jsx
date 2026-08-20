91a0bb0753488d612f05dc265cc6686069b8f79b
Marine-Briet <marinebriet@hotmail.com>
2026-08-20 15:41:24 +0200

    Components ‘card’ and ‘button’ #25

--- client/src/pages/Accueil.jsx
+++ client/src/pages/Accueil.jsx
@@ -1,5 +1,18 @@
+import Card from "../components/Card";
+import Bouton from "../components/Bouton";
+
 function Accueil() {
-    return <h1>Accueil</h1>
-};
+    return(
+    <Card>
+        <h1>Accueil</h1>
+        <Bouton>Se connecter</Bouton>
+        <br />
+        <br />
+        <Bouton variant="filtre" actif={true}>Corps & Quotidien</Bouton>
+        <br />
+        <br />
+        <Bouton variant="filtre" actif={false}>Adjectif</Bouton>
+    </Card>
+)};
 
 export default Accueil;
