eed4a8198faf2ccb13ade1610f3a7abb182c3568
Marine-Briet <marinebriet@hotmail.com>
2026-08-27 18:07:00 +0200

    Fix style and cleaning files #27

--- client/src/pages/Inscription.jsx
+++ client/src/pages/Inscription.jsx
@@ -1,4 +1,3 @@
-import Card from "../components/Card";
 import Header from "../components/Header";
 import Champ from "../components/Champ";
 import Bouton from "../components/Bouton";
@@ -46,9 +45,9 @@ function Inscription() {
             <Header simple/>
             <div className="page-centree">
                 <form onSubmit={gererInscription}>
-                    <Card className="carte-formulaire">
+                    <div className="carte-formulaire">
                         <div className="formulaire">
-                            <h1 className="titre-formulaire-inscription">Inscription</h1>
+                            <p className="titre-formulaire-inscription">Inscription</p>
                             <Champ label="Inscrire votre adresse mail" type="email" valeur={email} onChange={(e) => setEmail(e.target.value)} placeholder="votre adresse mail..." />
                             <Champ label="Créez un mot de passe" type="password" valeur={motDePasse} onChange={(e) => setMotDePasse(e.target.value)} placeholder="votre mot de passe..." />
                             <p className="mdp-aide">Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial (ex: !@#$%^&*)</p>
@@ -60,7 +59,7 @@ function Inscription() {
                             <Bouton type="submit" >JE M'INSCRIS</Bouton>
                             <p className="lien-secondaire">Déjà un compte ? <NavLink to="/connexion">Connectez-vous</NavLink></p>
                         </div>
-                    </Card>
+                    </div>
                 </form>
             </div>
         </div>
